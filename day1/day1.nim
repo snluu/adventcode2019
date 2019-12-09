36d1bb47a4a21c4a77fa030f61293677592100b5
Steven Luu <me@luu.io>
2019-12-08 21:48:13 -0800

    Impl part 2 of day 1

--- day1/day1.nim
+++ day1/day1.nim
@@ -1,18 +1,29 @@
 from parseutils import parseBiggestInt
 
+proc calculateFuelForFuel(fuel: int64): int64 =
+    var fuelRequired = (fuel div 3) - 2
+    while fuelRequired > 0:
+        result += fuelRequired
+        fuelRequired = (fuelRequired div 3) - 2
+
 proc main =
     let f = open("input.txt", fmRead)
     defer: f.close()
 
     var
         line: string
-        totalFuel = 0'i64
+        fuelForParts = 0'i64
+        fuelForFuel = 0'i64
     while f.readLine(line):
         var mass: int64
         if parseBiggestInt(line, mass) > 0:
-            totalFuel += (mass div 3) - 2
+            let fuel = (mass div 3) - 2
+            fuelForParts += fuel
+            fuelForFuel += calculateFuelForFuel(fuel)
 
-    echo "Total fuel: ", totalFuel
+    echo "Fuel for parts: ", fuelForParts
+    echo "Fuel for fuel: ", fuelForFuel
+    echo "Total: ", fuelForParts + fuelForFuel
 
 
 when isMainModule:
