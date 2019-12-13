9c62c2f60a8dcb547e02dd863d620f5f1092a450
Steven Luu <me@luu.io>
2019-12-13 11:56:02 -0800

    Day 11

--- day11/day11.nim
+++ day11/day11.nim
@@ -1,10 +1,18 @@
 import strutils
 import deques
+import tables
 
 var OpsParamCount: array[0..99, int]
 const PosHalted = -1'i64
 const PosNoInput = -2'i64
 const ProgramSize = 10240
+
+# up, right, down, left
+# Turn right should go to the next movement (wrapped around)
+# Turn left should go to the previous movement
+const Directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+const Colors = ["BLACK", "WHITE"]
+
 type
   Program = object
     name: string
@@ -14,6 +22,10 @@ type
     pos: int64
     relativeBase: int64
     halted: bool
+
+  PaintState = object
+    count: int
+    color: int
 
 proc initProgram(codes: array[ProgramSize, int64]): Program =
   result.codes = codes
@@ -129,6 +141,60 @@ proc execProgram(prog: var Program): int64 =
   while result >= 0 and not prog.halted:
     result = execCode(prog)
 
+proc getColor(
+  x, y, : int,
+  map: Table[int, Table[int, PaintState]],
+): int =
+  if map.hasKey(x) and map[x].hasKey(y):
+    result = map[x][y].color
+
+proc paint(
+  x, y, color: int,
+  map: var Table[int, Table[int, PaintState]],
+) =
+  # echo "Painting ", Colors[color], " at ", x, ",", y
+  discard map.hasKeyOrPut(x, initTable[int, PaintState]())
+  discard map[x].hasKeyOrPut(y, PaintState())
+
+  map[x][y].count += 1
+  map[x][y].color = color
+
+proc runPainter(
+  codes: array[ProgramSize, int64],
+  initialColor: int,
+): Table[int, Table[int, PaintState]] =
+  # Returns a # map<x, map<y, state>>
+
+  paint(0, 0, initialColor, result)
+  # don't want to count this againt output driven paint
+  result[0][0].count = 0
+
+  var prog = initProgram(codes)
+
+  var x, y, direction = 0
+  while not prog.halted:
+    let returnCode = execProgram(prog)
+    while prog.outputs.len != 0:
+      let color = prog.outputs.popFirst()
+      paint(x, y, int(color), result)
+
+      let rotation = prog.outputs.popFirst()
+      direction += (if rotation == 0: -1 else: 1)
+      if direction > high(Directions):
+        direction = low(Directions)
+      if direction < low(Directions):
+        direction = high(Directions)
+
+      x += Directions[direction][0]
+      y += Directions[direction][1]
+
+      prog.inputs.addLast(getColor(x, y, result))
+
+    if returnCode == PosNoInput and prog.inputs.len == 0:
+      prog.inputs.addLast(getColor(x, y, result))
+
+    assert returnCode != PosNoInput or prog.inputs.len > 0
+
 proc main =
   initOpsParams()
   let rawInput = readFile("input.txt")
@@ -137,11 +203,35 @@ proc main =
   for i, n in inputs:
     codes[i] = parseBiggestInt(n)
 
-  var prog = initProgram(codes)
-  prog.inputs.addLast(2)
+  # Part 1
+  let map = runPainter(codes, 0) #initial black tile
+  var total = 0
+  for ys in map.values:
+    total += ys.len
+  echo "Part 1 total: ", total
 
-  assert execProgram(prog) == -1
-  echo prog.outputs
+  # Part 2
+  let map2 = runPainter(codes, 1) # initial white tile
+  var
+    minX = high(int)
+    minY = high(int)
+    maxX = low(int)
+    maxY = low(int)
+
+  # find the top left and bottom right corners
+  for x in map2.keys:
+    for y in map2[x].keys:
+      minX = min(minX, x)
+      minY = min(minY, y)
+      maxX = max(maxX, x)
+      maxY = max(maxY, y)
+
+  echo "Part 2: "
+  for y in minY..maxY:
+    for x in minX..maxX:
+      let color = getColor(x, y, map2)
+      write(stdout, if color == 1: '#' else: ' ')
+    echo ""
 
 when isMainModule:
   main()
